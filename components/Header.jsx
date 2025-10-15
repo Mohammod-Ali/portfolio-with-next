4a8e856d1fc4706fbcea82b9bcc96f9c826b4d8b
Mohammod Ali <mohammodali651@gmail.com>
2025-10-16 01:26:48 +0600

    navbar ready

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,12 +1,24 @@
 import Link from "next/link";
 import { Button } from "./ui/button";
+import Nav from "./Nav";
 
 
 const Header = () => {
     return (
         <header className="py-8 xl:py-12 text-white">
-            <div className="container mx-auto">
-                <Link href={'/'}> <h1 className="text-4xl font-semibold">Mohammod Ali<span className="text-accent">.</span></h1></Link>
+            <div className="container mx-auto flex justify-between items-center">
+                {/* logo */}
+                <Link href={'/'}> <h1 className="text-4xl font-semibold hover:text-accent transition-all">Mohammod Ali<span className="text-accent">.</span></h1></Link>
+                {/* Desktop nav */}
+                <div className="hidden xl:flex items-center gap-8">
+                <Nav></Nav>
+                <Link href={'/contact'}> <button>Hire  Me</button></Link>
+                </div>
+                {/* mobile nav */}
+                <div className="xl:hidden ">
+                    mobile
+                </div>
+
             </div>
         </header>
     );
