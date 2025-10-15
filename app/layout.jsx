a9f0d1240f8948cfd6d09ee8f25a1cdc6ca83984
Mohammod Ali <mohammodali651@gmail.com>
2025-10-16 02:35:56 +0600

    framer motion added

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,7 @@ import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import PageTransition from "@/components/PageTransition";
+import StairTransition from "@/components/StairTransition";
 
 const jetBrains_Mono = JetBrains_Mono({
   subsets: ["latin"],
@@ -19,6 +20,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={`${jetBrains_Mono.variable}  antialiased`}>
         <Header></Header>
+        <StairTransition></StairTransition>
         <PageTransition>{children}</PageTransition>
       </body>
     </html>
