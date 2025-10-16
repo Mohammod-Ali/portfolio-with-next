52e61409bd9e013c2276953c9cf3e258b9d01d71
Mohammod Ali <mohammodali651@gmail.com>
2025-10-17 03:25:45 +0600

    mobile responsive navbar ready

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -38,7 +38,17 @@ const MobileNav = () => {
             </SheetTrigger>
             <SheetContent className={'flex flex-auto'}>
                {/* logo */}
-                <div>Logo</div>
+                <div className='mt-24 mb-14 text-center text-2xl'>
+                    <Link href={'/'}> <h1 className='text-accent text-4xl font-semibold'>Ali</h1> </Link>
+                </div>
+
+                <nav className='flex flex-col justify-center items-center gap-8'>
+                    {
+                        links.map((link, index) => {
+                            return <Link href={link.path} key={index} className={`${link.path === pathname && 'text-accent border-b-2 border-accent'} text-xl capitalize hover:text-accent transition-all`}>{link.name}</Link>
+                        })
+                    }
+                </nav>
             </SheetContent>
         </Sheet>
     );
