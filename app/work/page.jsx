68b56d51f11603a9583cd72e6de1f21493aac717
Mohammod Ali <mohammodali651@gmail.com>
2025-11-12 01:43:16 +0600

    work page build start and layout done

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,10 +1,72 @@
+"use client";
+
+import { motion } from "framer-motion";
+import React, { useState } from "react";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { BsArrowUpRight, BsGithub } from "react-icons/bs";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import Link from "next/link";
+import Image from "next/image";
+
+const projects = [
+  {
+    num: "01",
+    category: "Frontend",
+    title: "Project 1",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias magni officiis exercitationem nihil veritatis maiores eos ea saepe optio tempora aliquid aut",
+    stack: [{ name: "HTML 5" }, { name: "CSS 3" }, { name: "JavaScript" }],
+    image: "/assets/work/thumb1.png",
+    live: "",
+    github: "",
+  },
+  {
+    num: "02",
+    category: "Frontend",
+    title: "Project 2",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias magni officiis exercitationem nihil veritatis maiores eos ea saepe optio tempora aliquid aut",
+    stack: [{ name: "HTML 5" }, { name: "CSS 3" }, { name: "JavaScript" }],
+    image: "/assets/work/thumb2.png",
+    live: "",
+    github: "",
+  },
+  {
+    num: "03",
+    category: "Frontend",
+    title: "Project 3",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias magni officiis exercitationem nihil veritatis maiores eos ea saepe optio tempora aliquid aut",
+    stack: [{ name: "HTML 5" }, { name: "CSS 3" }, { name: "JavaScript" }],
+    image: "/assets/work/thumb3.png",
+    live: "",
+    github: "",
+  },
+];
 
 const Work = () => {
-    return (
-        <div>
-            Work
+  const [project, setProject] = useState(projects[0]);
+  return (
+    <motion.section initial={{opacity: 0}} animate={{opacity: 1}} className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0">
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row xl:gap-[30px]">
+            <div>
+                text
+            </div>
+            <div>
+                Slider
+            </div>
         </div>
-    );
+      </div>
+    </motion.section>
+  );
 };
 
 export default Work;
