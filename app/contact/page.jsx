cc834c75e68c50f7c65e78fb33d3a6d2ab046d6e
Mohammod Ali <mohammodali651@gmail.com>
2025-11-14 23:53:47 +0600

    contact page info added

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,4 +1,28 @@
-import React from 'react';
+'use client';
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select";
+
+import { FaPhoneAlt , FaEnvelope, FaMapMarkedAlt } from "react-icons/fa";
+
+const info = [
+    {
+        icon: <FaPhoneAlt></FaPhoneAlt>,
+        title: 'Phone',
+        description: "+8801933900625"     
+    },
+    {
+        icon: <FaEnvelope></FaEnvelope>,
+        title: 'Email',
+        description: "mohammodali651@gmail.com"     
+    },
+    {
+        icon: <FaMapMarkedAlt></FaMapMarkedAlt>,
+        title: 'Address',
+        description: "Bangladesh"     
+    },
+]
 
 const Contact = () => {
     return (
